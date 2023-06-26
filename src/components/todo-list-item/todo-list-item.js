7259173e58b7da483b8de299d4eef28b3d15d4a5
AlekseyDobryshkin <wanted_09@bk.ru>
2023-06-26 16:34:15 +0300

    функциональные компоненты изменены на компоненты классов

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,32 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './todo-list-item.css';
 
-const ToDoListItem = ({ label, important = false }) => {
+export default class ToDoListItem extends Component {
+    render() {
+        const { label, important = false } = this.props;
 
-    const style = {
-        color: important ? 'tomato' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
-    }
+        const style = {
+            color: important ? 'tomato' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        }
 
-    return (
-        <span className="todo-list-item">
-            <span
-                className="todo-list-item-label"
-                style={style}>
-                {label}
+        return (
+            <span className="todo-list-item">
+                <span
+                    className="todo-list-item-label"
+                    style={style}>
+                    {label}
+                </span>
+
+                <button type="button"
+                    className="btn btn-outline-success btn-sm float-right">
+                    <i className="fa fa-exclamation" />
+                </button>
+
+                <button type="button"
+                    className="btn btn-outline-danger btn-sm float-right">
+                    <i className="fa fa-trash-o" />
+                </button>
             </span>
-
-            <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
-                <i className="fa fa-exclamation" />
-            </button>
-
-            <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
-                <i className="fa fa-trash-o" />
-            </button>
-        </span>
-    );
+        );
+    }
 };
-
-export default ToDoListItem;
